66f37fd22b3b606ca3d42635da0f1bde43193342
Thomas Woods <>
2019-02-14 10:52:12 -0500

    initial upload

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,39 @@
+var friendsdata = require("../data/friendsdata");
+
+module.exports = function(app) {
+    app.post("/api/friendsdata", function(req, res) {
+        var newfriend = req.body;
+    
+        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        newfriend.name = newfriend.name.trim();
+        newfriend.photo = newfriend.photo.trim();
+        newfriend.scores = newfriend.scores;
+    
+        console.log(newfriend);
+
+        var differentScore = 100;
+        var matchIndex = 0;
+        for(var j = 0; j<friendsdata.length; j++){
+            var currentScore = 0;
+            for(var i = 0; i<10; i++){
+                currentScore += Math.abs(friendsdata[j].scores[i]-newfriend.scores[i]);
+            }
+            if(currentScore<differentScore)
+            {
+                differentScore = currentScore;
+                matchIndex = j;
+            }
+            console.log(friendsdata[j].photo);
+        }
+
+
+
+        friendsdata.push(newfriend);
+
+        res.json(friendsdata[matchIndex]);
+    });
+
+    app.get("/api/friendsdata", function(req, res) {
+        return res.json(friendsdata);
+    });
+};
